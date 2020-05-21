639417aa3dc062ccb1067801aaa646c3f1cd10b0
Trinhdvt <trinhvideo123@gmail.com>
2020-05-22 00:24:23 +0700

    Update Controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,11 +4,21 @@ import Model.HTMLObject;
 import View.View;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Controller {
     private final View view;
+    private final JFileChooser fileChooser = new JFileChooser();
+    private final HashSet<Long> myThreadId = new HashSet<>();
     private HTMLObject myObject = null;
 
     public Controller(View view) {
@@ -17,6 +27,7 @@ public class Controller {
     }
 
     public void initController() {
+        initWindowListener();
         initMenuAction();
         view.getRunBtn().addActionListener(l -> {
             String input = view.getInput().trim();
@@ -27,13 +38,12 @@ public class Controller {
             myObject = new HTMLObject(preProcessInput(input), header, index);
             view.setOutput(myObject.toTable());
         });
-
     }
 
     private void initMenuAction() {
         view.getAboutMenu().addActionListener(l -> JOptionPane.showMessageDialog(view,
                 "Trinh-dvt", "About", JOptionPane.INFORMATION_MESSAGE));
-        view.getExitMenu().addActionListener(l -> view.dispose());
+        view.getExitMenu().addActionListener(l -> view.dispatchEvent(new WindowEvent(view, WindowEvent.WINDOW_CLOSING)));
         view.getGettingStartedMenu().addActionListener(l -> {
             File doc = new File("src/Doc.txt");
             String absPath = doc.getAbsolutePath();
@@ -44,6 +54,63 @@ public class Controller {
                 e.printStackTrace();
             }
         });
+        view.getExportResult().addActionListener(l -> {
+            if (fileChooser.showSaveDialog(view) == JFileChooser.APPROVE_OPTION) {
+                Thread t = new Thread(() -> {
+                    try {
+                        exportDataToFile(fileChooser.getSelectedFile());
+                    } catch (IOException e) {
+                        JOptionPane.showInputDialog(view, "Cannot export data",
+                                "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                });
+                t.setName("Export Data");
+                myThreadId.add(t.getId());
+                t.start();
+            }
+        });
+    }
+
+    private void initWindowListener() {
+        view.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int res;
+                final Set<Thread> threads = Thread.getAllStackTraces().keySet();
+                if (threads.stream().filter(t -> myThreadId.contains(t.getId())).toArray().length > 0) {
+                    res = JOptionPane.showConfirmDialog(view,
+                            "Something is running in background. Force to exit ?",
+                            null, JOptionPane.OK_CANCEL_OPTION);
+                    if (res == JOptionPane.CANCEL_OPTION)
+                        return;
+                } else
+                    res = JOptionPane.showConfirmDialog(view, "Do you want to exit!",
+                            "Exit Confirm", JOptionPane.OK_CANCEL_OPTION);
+                if (res == JOptionPane.OK_OPTION) {
+                    view.dispose();
+                }
+            }
+        });
+    }
+
+    private void exportDataToFile(File file) throws IOException {
+        String input = view.getInput().trim();
+        String output = view.getOutput().trim();
+        if (input.isEmpty() || output.isEmpty()) {
+            JOptionPane.showMessageDialog(view, "Nothing to export",
+                    "Export information", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
+            bw.write("* Time: ");
+            bw.write(Calendar.getInstance().getTime().toString() + "\n");
+            bw.write("* Input:\n");
+            bw.write("Array: " + Arrays.deepToString(myObject.getArr()) + "\n");
+            bw.write("Header: " + myObject.getHeader() + "\n");
+            bw.write("Index: " + myObject.getIndex() + "\n");
+            bw.write("* Output:\n");
+            bw.write(view.getOutput());
+        }
     }
 
     private String[][] preProcessInput(String input) {
