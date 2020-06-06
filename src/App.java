266051442534688ffa39ec6ce897b559f4396c3d
Trinhdvt <trinhvideo123@gmail.com>
2020-06-06 15:49:37 +0700

    Modified

--- src/App.java
+++ src/App.java
@@ -1,15 +1,9 @@
 import Controller.Controller;
-import Model.HTMLObjectTableModel;
-import View.View;
 
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Thread(() -> {
-            View view = new View();
-            HTMLObjectTableModel model = new HTMLObjectTableModel();
-            new Controller(view, model);
-        }));
+        SwingUtilities.invokeLater(new Thread(() -> new Controller()));
     }
 }
